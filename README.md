eccfa69f30ca5b1018782b74941440573bbf85db
dev-mrg <62284621+dev-mrg@users.noreply.github.com>
2020-04-19 00:15:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# canvas-javascript-template
+Set up to quickly get moving on a HTML5 canvas project. 
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+let c = document.getElementById("canvas");
+let ctx = c.getContext("2d");
+c.width = innerWidth;
+c.height = innerHeight;
+let w = innerWidth;
+let h = innerHeight;
+
+
